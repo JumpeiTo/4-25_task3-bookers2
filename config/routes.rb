64df6d9cbe3c8ff91598d569bb8f040b30b86d44
EC2 Default User <ec2-user@ip-172-31-9-250.ap-northeast-1.compute.internal>
2023-05-15 09:39:06 +0000

    [finish]4-25-bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
    
   # ルートページ
   root to: "homes#top"
-  get '/homes/about' => 'homes#about' , as: 'about'
+  get '/home/about' => 'homes#about' , as: 'about'
   
   resources :books, only: [:new, :index, :show, :create, :destroy, :edit ,:update]
   resources :users, only: [:show, :edit, :update, :index]
